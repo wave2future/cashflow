250faf3a28a49a85d0bca7cbff3c8c1b44de0b45
tmurakam <tmurakam@a3d13447-d96d-4cb3-9281-862927cfb533>
2008-10-29 06:30:00 +0000

    mv initdb.sql
    git-svn-id: https://icashflow.svn.sourceforge.net/svnroot/icashflow/trunk@53 a3d13447-d96d-4cb3-9281-862927cfb533

---
+++ Database/initdb.sql
@@ -0,0 +1,20 @@
+CREATE TABLE Assets (
+        key INTEGER PRIMARY KEY,
+        type INTEGER,   /* 0:Cash, 1:Bank account, 2:Credit Card, 99:Other */
+        name TEXT);
+
+CREATE TABLE Transactions (
+        key INTEGER PRIMARY KEY,
+        asset INTEGER,  /* key of Assets */
+        date DATE,
+        type INTEGER,   /* 0:Outgo, 1:Income, 2:Adjustment */
+        value REAL,
+        balance REAL,
+        description TEXT,
+        memo TEXT);
+
+CREATE TABLE Category (
+        key INTEGER PRIMARY KEY,
+        sorder INTEGER,      /* sort order */
+        type INTEGER,   /* same as type of Transactions */
+        name TEXT);
