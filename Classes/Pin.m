7c4edf4006193f28f84aa66b0a49e8ea913db1eb
tmurakam <tmurakam@a3d13447-d96d-4cb3-9281-862927cfb533>
2009-01-14 04:08:49 +0000

    updated
    git-svn-id: https://icashflow.svn.sourceforge.net/svnroot/icashflow/trunk@282 a3d13447-d96d-4cb3-9281-862927cfb533

---
+++ Classes/Pin.m
@@ -0,0 +1,149 @@
+// -*-  Mode:ObjC; c-basic-offset:4; tab-width:8; indent-tabs-mode:nil -*-
+/*
+  CashFlow for iPhone/iPod touch
+
+  Copyright (c) 2008, Takuya Murakami, All rights reserved.
+
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are
+  met:
+
+  1. Redistributions of source code must retain the above copyright notice,
+  this list of conditions and the following disclaimer. 
+
+  2. Redistributions in binary form must reproduce the above copyright
+  notice, this list of conditions and the following disclaimer in the
+  documentation and/or other materials provided with the distribution. 
+
+  3. Neither the name of the project nor the names of its contributors
+  may be used to endorse or promote products derived from this software
+  without specific prior written permission. 
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+  PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#import "Pin.h"
+#import "PinVC.h"
+#import "AppDelegate.h"
+
+@implementation PinController
+
+- (void)firstPinCheck
+{
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    NSString *pin = [defaults stringForKey:@"PinCode"];
+    if (pin == nil || pin.length == 0) return; // do nothing
+
+    PinViewControlelr *vc = [self _getPinViewController];
+
+    vc.title = NSLocalizedString(@"Enter PIN", @"");
+    vc.enableCancel = NO;
+    vc.pin = pin;
+    vc.delegate = self;
+
+    state = ENTER_FIRST_PIN;
+
+    [navigationController pushViewController:vc animated:NO];
+}
+
+- (void)modifyPin
+{
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    self.pin = [defaults stringForKey:@"PinCode"];
+
+    vc = [self _getPinViewController];
+    vc.delegate = self;
+
+    if (pin != nil && pin.length > 0) {
+        // check current pin
+        state = ENTER_CURRENT_PIN;
+        vc.title = NSLocalizedString(@"Enter PIN", @"");
+        vc.pin = pin;
+    } else {
+        // enter 1st pin
+        state = ENTER_NEW_PIN1;
+        vc.title = NSLocalizedString(@"Enter new PIN", @"");
+    }
+        
+    [navigationController pushViewController:vc animated:NO];
+}
+
+- (BOOL)pinViewFinished:(PinViewController *)vc isCancel:(BOOL)isCancel
+{
+    BOOL result = YES;
+
+    if (isCancel) return result;
+
+    PinViewController *newvc = nil;
+
+    switch (state) {
+    case ENTER_FIRST_PIN:
+        if (![vc.value isEqualToString:self.pin]) {
+            // invalid pin
+            UIAlertView *v = [[UIAlertView alloc]
+                                 initWithTitle:@"Invalid PIN"
+                                 message:NSLocalizedString(@"PIN code does not match.", @"")
+                                 delegate:nil
+                                 cancelButtonTitle:@"Close"
+                                 otherButtonTitles:nil];
+            result = NO;
+        }
+        break;
+
+    case ENTER_CURRENT_PIN:
+        state = ENTER_NEW_PIN1;
+        newvc = [self _getPinViewController];        
+        newvc.title = NSLocalizedString(@"Enter new PIN", @"");
+        break;
+
+    case ENTER_NEW_PIN1:
+        newPin = vc.value;
+        state = ENTER_NEW_PIN2;
+        newvc = [self _getPinViewController];        
+        newvc.title = NSLocalizedString(@"Retype new PIN", @"");
+        break;
+
+    case ENTER_NEW_PIN2:
+        if ([vc.value isEqualToString:newPin]) {
+            // set new pin
+            NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+            [defaults setObject:newPin forKey:@"PinCode"];
+        } else {
+            // invalid pin
+            UIAlertView *v = [[UIAlertView alloc]
+                                 initWithTitle:@"Invalid PIN"
+                                 message:NSLocalizedString(@"PIN code does not match.", @"")
+                                 delegate:nil
+                                 cancelButtonTitle:@"Close"
+                                 otherButtonTitles:nil];
+        }
+        break;
+    }
+
+    if (newvc) {
+        newvc.delegate = self;
+        [navigationController pushViewController:newvc animated:YES];
+    }
+
+    return result;
+}
+
+
+- (PinViewController *)_getPinViewController
+{
+    PinViewController *vc = [[PinViewController alloc] initWithNibName:@"PinView.xib" bundle:nil];
+    [vc autorelease];
+    return vc;
+}
+
+@end
