a68f7ab74d7eb925035eb8a3cd30bfaed7586e9f
tmurakam <tmurakam@a3d13447-d96d-4cb3-9281-862927cfb533>
2009-02-26 11:05:37 +0000

    updated
    git-svn-id: https://icashflow.svn.sourceforge.net/svnroot/icashflow/trunk@320 a3d13447-d96d-4cb3-9281-862927cfb533

--- Classes/AssetListVC.m
+++ Classes/AssetListVC.m
@@ -463,7 +463,7 @@
 }
 
 - (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning]; // Releases the view if it doesn't have a superview
+    //[super didReceiveMemoryWarning]; // Releases the view if it doesn't have a superview
     // Release anything that's not essential, such as cached data
 }
 
--- Classes/TransactionListVC.m
+++ Classes/TransactionListVC.m
@@ -400,8 +400,10 @@
 }
 
 - (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning]; // Releases the view if it doesn't have a superview
-    // Release anything that's not essential, such as cached data
+    [super didReceiveMemoryWarning];
+
+    [transactionView release]; transactionView = nil;
+    [exportVC release]; exportVC = nil;
 }
 
 @end
