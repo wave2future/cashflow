182d400387ed37609745a9f308868e51f9661838
tmurakam <tmurakam@a3d13447-d96d-4cb3-9281-862927cfb533>
2009-03-26 01:37:52 +0000

    updated
    git-svn-id: https://icashflow.svn.sourceforge.net/svnroot/icashflow/trunk@334 a3d13447-d96d-4cb3-9281-862927cfb533

--- Classes/DateFormatter2.m
+++ Classes/DateFormatter2.m
@@ -49,25 +49,36 @@
 // 12時間制になってしまっているデータを強制的に変換する
 - (NSString *)fixDateString:(NSString *)string
 {
+    int hoffset = 0;
     NSRange range;
+    BOOL needFix = NO;
 
     // "午前"はそのまま削除
     range = [string rangeOfString:@"午前"];
     if (range.location != NSNotFound) {
-        string = [string stringByReplacingCharactersInRange:range withString:@""];
+        needFix = YES;
+        hoffset = 0;
+    } else {
+        range = [string rangeOfString:@"午後"];
+        if (range.location != NSNotFound) {
+            needFix = YES;
+            hoffset = 12;
+        }
     }
 
-    // "午後"は12時間ずらす
-    range = [string rangeOfString:@"午後"];
-    if (range.location != NSNotFound) {
+    if (needFix) {
         // 時刻を取り出す
         NSRange hrange = range;
         hrange.location += 2;
         hrange.length = 2;
         int hour = [[string substringWithRange] intValue];
 
-        // 時刻をずらす
-        hour += 12;
+        // 時刻を調整
+        if (hour == 12) {
+            // 午前12時 ⇒ 0時、午後12時 ⇒ 12時
+            hour = 0;
+        }
+        hour += hoffset;
         NSString *hstr = [NSString stringWithFormat:@"%02d", hour];
 
         // 文字列を置換
