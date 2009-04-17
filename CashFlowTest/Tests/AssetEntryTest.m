bc9c3940b120d1405bbbff4f07aec588eff33541
Takuya Murakami <tmurakam@tmurakam.org>
2009-04-17 23:18:18 +0900

    updated

--- CashFlowTest/Tests/AssetEntryTest.m
+++ CashFlowTest/Tests/AssetEntryTest.m
@@ -1,13 +1,6 @@
 // -*-  Mode:ObjC; c-basic-offset:4; tab-width:8; indent-tabs-mode:nil -*-
 
-#import "TestCommon.h"
-#import "DataModel.h"
-
-@interface AssetEntryTest : IUTTest {
-
-}
-
-@end
+#import "AssetEntryTest.h"
 
 @implementation AssetEntryTest
 
