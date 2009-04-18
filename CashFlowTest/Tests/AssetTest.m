929862aa1cfc8670e87f4d512eb9a45c6da78a8c
Takuya Murakami <tmurakam@tmurakam.org>
2009-04-18 14:11:29 +0900

    updated

--- CashFlowTest/Tests/AssetTest.m
+++ CashFlowTest/Tests/AssetTest.m
@@ -21,4 +21,60 @@
     [super tearDown];
 }
 
+- (void)testWithData
+{
+    [TestCommon installDatabase:@"testdata1"];
+    Ledger *ledger = [DataModel ledger];
+    AssetEntry *e;
+
+    asset = [ledger assetAtIndex:0];
+    ASSERT_EQUAL_INT(1, asset.pkey);
+    ASSERT_EQUAL_INT(0, asset.type);
+    ASSERT([asset.name isEqualToString:@"Cash"]);
+    ASSERT_EQUAL_INT(0, asset.sorder);
+    ASSERT_EQUAL_DOUBLE(5000, asset.initialBalance);
+    
+    ASSERT_EQUAL_INT(4, [asset entryCount]);
+    e = [asset entryAt:0];
+    ASSERT_EQUAL_DOUBLE(-100, e.value);
+    ASSERT_EQUAL_DOUBLE(4900, e.balance);
+    e = [asset entryAt:1];
+    ASSERT_EQUAL_DOUBLE(-800, e.value);
+    ASSERT_EQUAL_DOUBLE(4100, e.balance);
+    e = [asset entryAt:2];
+    ASSERT_EQUAL_DOUBLE(-100, e.value);
+    ASSERT_EQUAL_DOUBLE(4000, e.balance);
+    e = [asset entryAt:3];
+    ASSERT_EQUAL_DOUBLE(5000, e.value);
+    ASSERT_EQUAL_DOUBLE(9000, e.balance);
+
+    asset = [ledger assetAtIndex:1];
+    ASSERT_EQUAL_INT(2, asset.pkey);
+    ASSERT_EQUAL_INT(1, asset.type);
+    ASSERT([asset.name isEqualToString:@"Bank"]);
+    ASSERT_EQUAL_INT(1, asset.sorder);
+    ASSERT_EQUAL_DOUBLE(100000, asset.initialBalance);
+
+    ASSERT_EQUAL_INT(2, [asset entryCount]);
+    e = [asset entryAt:0];
+    ASSERT_EQUAL_DOUBLE(-5000, e.value);
+    ASSERT_EQUAL_DOUBLE(95000, e.balance);
+    e = [asset entryAt:1];
+    ASSERT_EQUAL_DOUBLE(100000, e.value);
+    ASSERT_EQUAL_DOUBLE(195000, e.balance);
+
+    asset = [ledger assetAtIndex:2];
+    ASSERT_EQUAL_INT(3, asset.pkey);
+    ASSERT_EQUAL_INT(2, asset.type);
+    ASSERT([asset.name isEqualToString:@"Card"]);
+    ASSERT_EQUAL_INT(2, asset.sorder);
+    ASSERT_EQUAL_DOUBLE(-10000, asset.initialBalance);
+    
+    ASSERT_EQUAL_INT(1, [asset entryCount]);
+
+    e = [asset entryAt:0];
+    ASSERT_EQUAL_DOUBLE( -2100, e.value);
+    ASSERT_EQUAL_DOUBLE(-12100, e.balance);
+}
+
 @end
--- CashFlowTest/Tests/LedgerTest.m
+++ CashFlowTest/Tests/LedgerTest.m
@@ -26,6 +26,28 @@
 {
     // 現金のみがあるはず
     ASSERT([ledger.assets count] == 1);
+    [ledger load];
+    [ledger rebuild];
+    ASSERT([ledger.assets count] == 1);
+
+    Asset *asset = [ledger assetAtIndex:0];
+    ASSERT_EQUAL_INT(0, [asset entryCount]);
+}
+
+- (void)testNormal
+{
+    [TestCommon installDatabase:@"testdata1"];
+    ledger = [DataModel ledger];
+    
+    // 現金のみがあるはず
+    ASSERT([ledger.assets count] == 3);
+    [ledger load];
+    [ledger rebuild];
+    ASSERT([ledger.assets count] == 3);
+
+    ASSERT_EQUAL_INT(4, [[ledger assetAtIndex:0] entryCount]);
+    ASSERT_EQUAL_INT(2, [[ledger assetAtIndex:1] entryCount]);
+    ASSERT_EQUAL_INT(1, [[ledger assetAtIndex:2] entryCount]);
 }
 
 @end
