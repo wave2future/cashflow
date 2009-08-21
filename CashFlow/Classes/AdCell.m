ad318fdab72e9a64f1e90b7b9ad9a254919db0c7
Takuya Murakami <tmurakam@tmurakam.org>
2009-08-22 01:55:20 +0900

    misc fix

--- CashFlow/Classes/AdCell.m
+++ CashFlow/Classes/AdCell.m
@@ -7,6 +7,44 @@
 
 #import "TGAView.h" // TG ad
 
+@interface AdMobDelegate : NSObject <AdMobDelegate> {
+}
+
++ (AdMobDelegate*)getInstance;
+@end
+
+@implementation AdMobDelegate
+
++ (AdMobDelegate*)getInstance
+{
+    static AdMobDelegate *theInstance = nil;
+    if (theInstance == nil) {
+        theInstance = [[AdMobDelegate alloc] init];
+    }
+    return theInstance;
+}
+
+- (NSString*)publisherId
+{
+    return @"a14a8b599ca8e92";
+}
+
+#if 1
+- (BOOL)useTestAd {
+    return YES;
+}
+#endif
+
+- (void)didReceiveAd:(AdMobView *)adView {
+    NSLog(@"AdMob:disReceiveAd");
+}
+
+- (void)didFailToReceiveAd:(AdMobView *)adView {
+    NSLog(@"AdMob:didFailToReceiveAd");
+}
+
+@end
+
 @implementation AdCell
 
 + (AdCell *)adCell:(UITableView *)tableView
@@ -24,21 +62,24 @@
 {
     self = [super initWithFrame:frame reuseIdentifier:identifier];
 
-#if 0
-    // AdMob
-    AdMobView *admob = [AdMobView requestAdWithDelegate:self];
-    [self.contentView addSubview:admob];
+    NSString *plang = [[NSLocale preferredLanguages] objectAtIndex:0];
+    //NSLog(@"Prefered Language: %@", plang);
 
-#else
-    // TG ad
-    static TGAView *tgad = nil;
-    if (tgad == nil) {
-        tgad = [TGAView requestWithKey:@"5AeoNWm3LatP" Position:0];
-        [tgad retain];
+    if ([plang isEqualToString:@"ja"]) {
+        // TG ad
+        static TGAView *tgad = nil;
+        if (tgad == nil) {
+            tgad = [TGAView requestWithKey:@"5AeoNWm3LatP" Position:0];
+            [tgad retain];
+        }
+        [self.contentView addSubview:tgad];
+        [tgad release];
+    } else {
+        // AdMob
+        AdMobDelegate *amd = [AdMobDelegate getInstance];
+        AdMobView *admob = [AdMobView requestAdWithDelegate:amd];
+        [self.contentView addSubview:admob];
     }
-    [self.contentView addSubview:tgad];
-    [tgad release];
-#endif
     
     return self;
 }
