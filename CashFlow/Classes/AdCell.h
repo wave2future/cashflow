92627d12c978c74d8a788ff17febf993fce68aea
Takuya Murakami <tmurakam@tmurakam.org>
2010-04-29 18:16:48 +0900

    updated

--- CashFlow/Classes/AdCell.h
+++ CashFlow/Classes/AdCell.h
@@ -12,6 +12,7 @@
 
 #define AFMA_CLIENT_ID  @"ca-mb-app-pub-4621925249922081"
 #define AFMA_CHANNEL_IDS @"7922983440"
+#define AFMA_CHANNEL_IDS_IPAD @"5863989042"
 #define AFMA_KEYWORDS  @"マネー,預金,キャッシュ,クレジット,小遣い,貯金,資産+管理,money,deposit,cash,credit,allowance,spending+money,pocket+money,savings,saving+money,asset+management"
 #define AFMA_IS_TEST 1
 
--- CashFlow/Classes/AdCell.m
+++ CashFlow/Classes/AdCell.m
@@ -9,6 +9,7 @@
 #if FREE_VERSION
 
 #import "AdCell.h"
+#import "AppDelegate.h"
 
 /////////////////////////////////////////////////////////////////////
 // AdCell
@@ -43,7 +44,6 @@
          @"Takuya Murakami", kGADAdSenseCompanyName,
          @"CashFlow Free", kGADAdSenseAppName,
          AFMA_KEYWORDS, kGADAdSenseKeywords,
-         [NSArray arrayWithObjects:AFMA_CHANNEL_IDS, nil], kGADAdSenseChannelIDs,
          [NSNumber numberWithInt:AFMA_IS_TEST], kGADAdSenseIsTestAdRequest,
 
          [UIColor whiteColor], kGADAdSenseAdBackgroundColor,
@@ -57,7 +57,17 @@
          [UIColor colorWithRed:0.3 green:0.3 blue:0.5 alpha:0], kGADAdSenseAdLinkColor,
          [UIColor colorWithRed:0.0 green:0.4 blue:0.0 alpha:0], kGADAdSenseAdURLColor,
          nil];
-    return attributes;
+
+    NSMutableDictionary *md = [[[NSMutableDictionary alloc] init] autorelease];
+    [md setDictionary:attributes];
+    
+    if (!IS_IPAD) {
+        [md setObject:[NSArray arrayWithObjects:AFMA_CHANNEL_IDS, nil] forKey:kGADAdSenseChannelIDs];
+    } else {
+        [md setObject:[NSArray arrayWithObjects:AFMA_CHANNEL_IDS_IPAD, nil] forKey:kGADAdSenseChannelIDs];
+    }
+
+    return md;
 }
 
 - (UITableViewCell *)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)identifier
