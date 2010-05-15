860aff1903d3f29177e6cf3f903781a2fe5be1a6
Takuya Murakami <tmurakam@tmurakam.org>
2010-05-15 23:20:25 +0900

    add test

---
+++ CashFlow/AppTests/AssetListVCTests.m
@@ -0,0 +1,56 @@
+// -*-  Mode:ObjC; c-basic-offset:4; tab-width:8; indent-tabs-mode:nil -*-
+
+#import "TestCommon.h"
+#import "AssetListVC.h"
+
+@interface AssetListViewControllerTest : UINavigationBarBasedTest {
+    AssetListViewController *vc;
+}
+@end
+
+@implementation AssetListViewControllerTest
+
+- (UIViewController *)rootViewController
+{
+    vc = [[[AssetListViewController alloc] initWithNibName:@"AssetListView" bundle:nil] autorelease];
+    return vc;
+}
+
+- (void)setUp
+{
+    [TestCommon installDatabase:@"testdata1"];
+
+    // AssetView を表示させないようにガードする
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    [defaults setInteger:-1 forKey:@"firstShowAssetIndex"];
+    
+    [super setUp];
+ }
+
+- (void)tearDown
+{
+    [super tearDown];
+}
+
+- (NSString *)cellText:(int)row section:(int)section
+{
+    NSIndexPath *index = [NSIndexPath indexPathForRow:row inSection:section];
+    UITableViewCell *cell = [vc tableView:vc.tableView cellForRowAtIndexPath:index];
+    NSLog(@"'%@'", cell.text);
+    return cell.text;
+}
+
+- (void)testNormal
+{
+    // test number of rows
+    ASSERT_EQUAL_INT(3, [vc tableView:vc.tableView numberOfRowsInSection:0]);
+    ASSERT_EQUAL_INT(1, [vc tableView:vc.tableView numberOfRowsInSection:1]); // 合計
+
+    // test cell
+    ASSERT_EQUAL(@"Cash : ￥9,000", [self cellText:0 section:0]);
+    ASSERT_EQUAL(@"Bank : ￥195,000", [self cellText:1 section:0]);
+    ASSERT_EQUAL(@"Card : -￥12,100", [self cellText:2 section:0]);
+    //ASSERT_EQUAL(@"計 : 10000", [self cellText:0 section:1]);
+}
+
+@end
---
+++ CashFlow/LogicTests/AssetEntryTests.m
@@ -0,0 +1,141 @@
+// -*-  Mode:ObjC; c-basic-offset:4; tab-width:8; indent-tabs-mode:nil -*-
+
+#import "TestCommon.h"
+
+@interface AssetEntryTest : SenTestCase {
+}
+
+@end
+
+
+@implementation AssetEntryTest
+
+- (void)setUp
+{
+}
+
+- (void)tearDown
+{
+}
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+
+#pragma mark -
+#pragma mark Helpers
+
+
+#pragma mark -
+#pragma mark Tests
+
+// transaction 指定なし
+- (void)testAllocNew
+{
+    AssetEntry *e;
+    Asset *a = [[[Asset alloc] init] autorelease];
+    a.pkey = 999;
+
+    e = [[[AssetEntry alloc] initWithTransaction:nil withAsset:a] autorelease];
+
+    Assert(e.assetKey == 999);
+    Assert(e.value == 0.0);
+    Assert(e.balance == 0.0);
+    Assert(e.transaction.asset == 999);
+    Assert(![e isDstAsset]);
+
+    // 値設定
+    e.value = 200.0;
+    //[e setupTransaction];
+    Assert(e.transaction.value == 200.0);
+}
+
+// transaction 指定あり、通常
+- (void)testAllocExisting
+{
+    Asset *a = [[[Asset alloc] init] autorelease];
+    a.pkey = 111;
+    Transaction *t = [[[Transaction alloc] init] autorelease];
+    t.type = TYPE_TRANSFER;
+    t.asset = 111;
+    t.dst_asset = 222;
+    t.value = 10000.0;
+
+    AssetEntry *e = [[[AssetEntry alloc] initWithTransaction:t withAsset:a] autorelease];
+
+    Assert(e.assetKey == 111);
+    Assert(e.value == 10000.0);
+    Assert(e.balance == 0.0);
+    Assert(e.transaction.asset == 111);
+    Assert(![e isDstAsset]);
+
+    // 値設定
+    e.value = 200.0;
+    //[e setupTransaction];
+    Assert(e.transaction.value == 200.0);
+}
+
+// transaction 指定あり、逆
+- (void)testAllocExistingReverse
+{
+    Asset *a = [[[Asset alloc] init] autorelease];
+    a.pkey = 111;
+    Transaction *t = [[[Transaction alloc] init] autorelease];
+    t.type = TYPE_TRANSFER;
+    t.asset = 222;
+    t.dst_asset = 111;
+    t.value = 10000.0;
+
+    AssetEntry *e = [[[AssetEntry alloc] initWithTransaction:t withAsset:a] autorelease];
+
+    Assert(e.assetKey == 111);
+    Assert(e.value == -10000.0);
+    Assert(e.balance == 0.0);
+    Assert(e.transaction.asset == 222);
+    Assert([e isDstAsset]);
+
+    // 値設定
+    e.value = 200.0;
+    //[e setupTransaction];
+    Assert(e.transaction.value == -200.0);
+}
+
+- (void)testEvalueNormal
+{
+    Asset *a = [[[Asset alloc] init] autorelease];
+    a.pkey = 111;
+    Transaction *t = [[[Transaction alloc] init] autorelease];
+    t.asset = 111;
+    t.dst_asset = -1;
+
+    AssetEntry *e = [[[AssetEntry alloc] initWithTransaction:t withAsset:a] autorelease];
+    e.balance = 99999.0;
+
+    t.type = TYPE_INCOME;
+    e.value = 10000;
+    Assert(e.evalue == 10000);
+    e.evalue = 20000;
+    Assert(e.transaction.value == 20000);    
+
+    t.type = TYPE_OUTGO;
+    e.value = 10000;
+    Assert(e.evalue == -10000);
+    e.evalue = 20000;
+    Assert(e.transaction.value = -20000);
+
+    t.type = TYPE_ADJ;
+    e.balance = 99999;
+    Assert([e evalue] == 99999);
+    e.evalue = 88888;
+    Assert(e.balance == 88888);
+
+    t.type = TYPE_TRANSFER;
+    e.value = 10000;
+    Assert([e evalue] == -10000);
+    e.evalue = 20000;
+    Assert(e.transaction.value == -20000);
+}
+
+@end
