37a9c2817e472fc4a09ca9593303955398b476bd
Takuya Murakami <tmurakam@tmurakam.org>
2010-07-12 19:47:36 +0900

    bug fix : inherit NSObject

--- CashFlow/Classes/DescLRUManager.m
+++ CashFlow/Classes/DescLRUManager.m
@@ -2,7 +2,7 @@
 /*
   CashFlow for iPhone/iPod touch
 
-  Copyright (c) 2008, Takuya Murakami, All rights reserved.
+  Copyright (c) 2008-2010, Takuya Murakami, All rights reserved.
 
   Redistribution and use in source and binary forms, with or without
   modification, are permitted provided that the following conditions are
