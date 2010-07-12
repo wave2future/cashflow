313d255421f377552f60aa11eb1af14b7fc85e93
Takuya Murakami <tmurakam@tmurakam.org>
2010-07-13 00:06:00 +0900

    add migration test

--- CashFlow/LogicTests/TransactionTests.m
+++ CashFlow/LogicTests/TransactionTests.m
@@ -11,10 +11,32 @@
 
 - (void)setUp
 {
+    [super setUp];
+    [TestCommon deleteDatabase];
+    [DataModel instance];
 }
 
 - (void)tearDown
 {
+    [super tearDown];
+}
+
+// 日付のアップグレードテスト (ver 3.2.1 -> 3.3 へのアップグレード)
+- (void)testMigrateDate
+{
+    Database *db = [Database instance];
+    
+    // 旧バージョンのフォーマットでデータを作成
+    [db exec:@"INSERT INTO Transactions VALUES(NULL, 0, 0, 200901011256, 0, 0, 0, '', '');"];
+    
+    // Migrate 実行
+    [DataModel finalize];
+    [DataModel instance];
+    
+    // チェック
+    dbstmt *stmt = [db prepare:@"SELECT date FROM Transactions;"];
+    STAssertTrue([stmt step] == SQLITE_ROW, @"select failed.");
+    STAssertTrue([[stmt colString:0] isEqualToString:@"20090101125600"], @"date migration failed");
 }
 
 @end
