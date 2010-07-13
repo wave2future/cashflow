590c2dde7f449f218bbde5511f5e2a41aa3d08b0
Takuya Murakami <tmurakam@tmurakam.org>
2010-07-13 18:48:08 +0900

    add tests

--- CashFlow/AppTests/EditDescVCTests.m
+++ CashFlow/AppTests/EditDescVCTests.m
@@ -5,28 +5,42 @@
 #import "DescLRU.h"
 #import "DescLRUManager.h"
 
-@interface EditDescViewControllerTest : SenTestCase {
+@interface EditDescViewControllerTest : SenTestCase <EditDescViewDelegate> {
     EditDescViewController *vc;
+    NSString *description;
 }
 @end
 
 @implementation EditDescViewControllerTest
 
-- (UIViewController *)rootViewController
+#pragma mark EditDescViewDelegate
+
+- (void)editDescViewChanged:(EditDescViewController*)vc
 {
-    vc = [[[AssetListViewController alloc] initWithNibName:@"AssetListView" bundle:nil] autorelease];
-    return vc;
+    [description release];
+    description = vc.description;
+}
+
+#pragma mark -
+
+- (void)dealloc
+{
+    [description release];
+    [super dealloc];
 }
 
 - (void)setUp
 {
+    description = nil;
+
     // erase all desc LRU data
     [DescLRU delete_cond:nil];
 
     vc = [[[EditDescViewController alloc] initWithNibName:@"EditDescView" bundle:nil] autorelease];
 
-    vc.description = @"hoge";
+    vc.description = @"TEST";
     vc.category = 100;
+    vc.delegate = self;
 
     [vc viewDidLoad];
     [vc viewWillAppear:YES];
@@ -35,6 +49,7 @@
 - (void)tearDown
 {
     [vc viewWillDisappear:YES];
+    [description release];
 }
 
 - (UITableViewCell *)_cellForRow:(int)row section:(int)section
@@ -52,7 +67,8 @@
     UITableViewCell *cell = [self _cellForRow:0 section:0];
     Assert(cell != nil);
 
-    // TBD: test textField
+    [vc doneAction];
+    Assert([description isEqualToString:@"TEST"]);
 }
 
 - (void)testEmptyLRU
@@ -103,6 +119,23 @@
     Assert([cell.textLabel.text isEqualToString:@"test4"]);
     cell = [self _cellForRow:1 section:1];
     Assert([cell.textLabel.text isEqualToString:@"test1"]);
+}
+
+- (void)testClickCell
+{
+    [DescLRUManager addDescLRU:@"test0" category:0 date:[db dateFromString:@"20100101000000"]];
+    [DescLRUManager addDescLRU:@"test1" category:1 date:[db dateFromString:@"20100101000001"]];
+    [DescLRUManager addDescLRU:@"test2" category:2 date:[db dateFromString:@"20100101000002"]];
+    [DescLRUManager addDescLRU:@"test3" category:0 date:[db dateFromString:@"20100101000003"]];
+    [DescLRUManager addDescLRU:@"test4" category:1 date:[db dateFromString:@"20100101000004"]];
+    [DescLRUManager addDescLRU:@"test5" category:2 date:[db dateFromString:@"20100101000005"]];
+
+    vc.category = -1;
+    [vc viewWillAppear:YES]; // reload descArray
+
+    NSIndexPath *indexPath = [NSIndexPath pathForRow:1 forSection:1];
+    [vc tableView:vc.tableView didSelectRowAtIndexPath:indexPath];
+    Assert([description isEqualToString:@"test4"]);
 }
 
 @end
