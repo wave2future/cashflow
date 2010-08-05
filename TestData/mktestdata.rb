42f6da8c502c4751170b2a2af495f60a36024341
Takuya Murakami <tmurakam@tmurakam.org>
2010-08-05 20:47:33 +0900

    add mktestdata

---
+++ TestData/mktestdata.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/ruby
+
+def initTable
+    puts "CREATE TABLE Transactions (key INTEGER PRIMARY KEY,asset INTEGER,dst_asset INTEGER,date DATE,type INTEGER,category INTEGER,value REAL,description TEXT,memo TEXT);"
+    puts "CREATE TABLE Assets (key INTEGER PRIMARY KEY,name TEXT,type INTEGER,initialBalance REAL,sorder INTEGER);"
+    puts "CREATE TABLE Categories (key INTEGER PRIMARY KEY,name TEXT,sorder INTEGER);"
+end
+
+def initAssets
+    puts <<EOF
+INSERT INTO "Assets" VALUES(1,'現金',0,7000.0,99999);
+INSERT INTO "Assets" VALUES(2,'ABC銀行',1,500000.0,99999);
+EOF
+end
+
+def initCategories
+    puts <<EOF
+INSERT INTO "Categories" VALUES(1,'食費',0);
+INSERT INTO "Categories" VALUES(2,'交通費',1);
+EOF
+end
+
+def createTransactions
+    puts "INSERT INTO \"Transactions\" VALUES(1,2,1,201004010236,3,-1,-20000.0,'ATM','');"
+end
+
+
+puts "BEGIN TRANSACTION;"
+initTable
+initAssets
+initCategories
+createTransactions
+puts "COMMIT;"
