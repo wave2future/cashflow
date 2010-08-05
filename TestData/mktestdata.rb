c39ced883b9c8ada24d17d23864e008a3ec5f89d
Takuya Murakami <tmurakam@tmurakam.org>
2010-08-05 20:57:52 +0900

    updated

--- TestData/mktestdata.rb
+++ TestData/mktestdata.rb
@@ -1,5 +1,8 @@
 #!/usr/bin/ruby
 
+MAX_ASSET = 4
+MAX_CATEGORY = 5
+
 def initTable
     puts "CREATE TABLE Transactions (key INTEGER PRIMARY KEY,asset INTEGER,dst_asset INTEGER,date DATE,type INTEGER,category INTEGER,value REAL,description TEXT,memo TEXT);"
     puts "CREATE TABLE Assets (key INTEGER PRIMARY KEY,name TEXT,type INTEGER,initialBalance REAL,sorder INTEGER);"
@@ -10,6 +13,8 @@ def initAssets
     puts <<EOF
 INSERT INTO "Assets" VALUES(1,'現金',0,7000.0,99999);
 INSERT INTO "Assets" VALUES(2,'ABC銀行',1,500000.0,99999);
+INSERT INTO "Assets" VALUES(3,'XYZカード',1,-30000.0,99999);
+INSERT INTO "Assets" VALUES(4,'xxxカード',1,-20000.0,99999);
 EOF
 end
 
@@ -17,6 +22,7 @@ def initCategories
     puts <<EOF
 INSERT INTO "Categories" VALUES(1,'食費',0);
 INSERT INTO "Categories" VALUES(2,'交通費',1);
+INSERT INTO "Categories" VALUES(3,'医療費',2);
 EOF
 end
 
@@ -30,9 +36,17 @@ def createTransactions
     min = 0
     sec = 0
 
-    while (pkey < 
+    while (pkey < 1000)
 
-    puts "INSERT INTO \"Transactions\" VALUES(1,2,1,201004010236,3,-1,-20000.0,'ATM','');"
+        d = sprintf("%04d%02d%02d%02d%02d%02d", year, month, day, hour, min, sec);
+        type = pkey % 3 + 1
+        asset = pkey % MAX_ASSET + 1
+        cat = pkey % MAX_CATEGORY + 1
+
+        puts "INSERT INTO \"Transactions\" VALUES(#{pkey},#{asset},-1,#{d},#{type},#{cat},#{pkey*10.0},'hoge','xxx');"
+
+        pkey += 1
+    end
 end
 
 
