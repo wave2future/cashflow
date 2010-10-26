4193df0bee883d464750255926a06d0cf56ee9c5
Takuya Murakami <tmurakam@tmurakam.org>
2010-10-26 22:09:11 +0900

    bug fixed

--- android/src/org/tmurakam/cashflow/CurrencyManager.java
+++ android/src/org/tmurakam/cashflow/CurrencyManager.java
@@ -1,4 +1,4 @@
-// -*-  Mode:java; c-basic-offset:4; tab-width:8; indent-tabs-mode:nil -*-
+// -*-  Mode:java; c-basic-offset:4; tab-width:4; indent-tabs-mode:t -*-
 
 package org.tmurakam.cashflow;
 
@@ -7,88 +7,88 @@ import java.util.*;
 import java.text.*;
 
 public class CurrencyManager {
-    public String baseCurrency;
+	public String baseCurrency;
 
-    private NumberFormat numberFormat;
+	private NumberFormat numberFormat;
 
     private static CurrencyManager theInstance = null;
 
     private static final String[] currencies = {
-         "AED",
-         "AUD",
-         "BHD",
-         "BND",
-         "BRL",
-         "CAD", 
-         "CHF",
-         "CLP",
-         "CNY",
-         "CYP",
-         "CZK",
-         "DKK",
-         "EUR",
-         "GBP",
-         "HKD",
-         "HUF",
-         "IDR",
-         "ILS",
-         "INR",
-         "ISK",
-         "JPY",
-         "KRW",
-         "KWD",
-         "KZT",
-         "LKR",
-         "MTL",
-         "MUR",
-         "MXN",
-         "MYR",
-         "NOK",
-         "NPR",
-         "NZD",
-         "OMR",
-         "PKR",
-         "QAR",
-         "RUB",
-         "SAR",
-         "SEK",
-         "SGD",
-         "SKK",
-         "THB",
-         "TWD",
-         "USD",
-         "ZAR"
+		"AED",
+		"AUD",
+		"BHD",
+		"BND",
+		"BRL",
+		"CAD", 
+		"CHF",
+		"CLP",
+		"CNY",
+		"CYP",
+		"CZK",
+		"DKK",
+		"EUR",
+		"GBP",
+		"HKD",
+		"HUF",
+		"IDR",
+		"ILS",
+		"INR",
+		"ISK",
+		"JPY",
+		"KRW",
+		"KWD",
+		"KZT",
+		"LKR",
+		"MTL",
+		"MUR",
+		"MXN",
+		"MYR",
+		"NOK",
+		"NPR",
+		"NZD",
+		"OMR",
+		"PKR",
+		"QAR",
+		"RUB",
+		"SAR",
+		"SEK",
+		"SGD",
+		"SKK",
+		"THB",
+		"TWD",
+		"USD",
+		"ZAR"
     };
 
     public static CurrencyManager instance() {
-	if (theInstance == null) {
-	    theInstance = new CurrencyManager();
-	}
-	return theInstance;
+		if (theInstance == null) {
+			theInstance = new CurrencyManager();
+		}
+		return theInstance;
     }
 
     public CurrencyManager() {
-	numberFormat = new NumberFormat.getCurrencyInstance();
+		numberFormat = NumberFormat.getCurrencyInstance();
 
-	// TBD
-	baseCurrency = [[NSUserDefaults standardUserDefaults] objectForKey:@"BaseCurrency"];
-	setBaseCurrency(baseCurrency);
+		// TBD
+		baseCurrency = [[NSUserDefaults standardUserDefaults] objectForKey:@"BaseCurrency"];
+		setBaseCurrency(baseCurrency);
     }
 
     public static String systemCurrency() {
-	return Currency.getInstance().getCurrencyCode();
+		return Currency.getInstance(Locale.getDefault()).getCurrencyCode();
     }
 
     public void setBaseCurrency(String currencyCode) {
-	baseCurrency = currencyCode;
+		baseCurrency = currencyCode;
         
-	Currency currency;
+		Currency currency;
         if (currencyCode == null) {
-	    currency = Currency.getInstance();
-	} else {
-	    currency = Currency.getInstance(currencyCode);
-	}
-	numberFormatter.setCurrency(currency);
+			currency = Currency.getInstance(Locale.getDefault());
+		} else {
+			currency = Currency.getInstance(currencyCode);
+		}
+		numberFormat.setCurrency(currency);
         
         [[NSUserDefaults standardUserDefaults] setObject:baseCurrency forKey:@"BaseCurrency"];
     }
@@ -101,5 +101,3 @@ public class CurrencyManager {
         return numberFormat.format(value);
     }
 }
-
-    
