81a5fefeec18079bb955ad6e985bc923386f55ed
Takuya Murakami <tmurakam@tmurakam.org>
2010-11-09 19:34:52 +0900

    split android repo

--- android/src/org/tmurakam/cashflow/models/CatReport.java
+++
@@ -1,8 +0,0 @@
-// -*-  Mode:java; c-basic-offset:4; tab-width:4; indent-tabs-mode:t -*-
-
-package org.tmurakam.cashflow.models;
-
-public class CatReport {
-	public int catkey; // カテゴリキー
-	public double value; // 合計値
-}
