4a145ad4f8d905891d66324ca54cf9bd53316030
Takuya Murakami <tmurakam@tmurakam.org>
2011-01-21 20:46:48 +0900

    utf-8/16

--- locale/utf8/backutf16.sh
+++ locale/utf8/backutf16.sh
@@ -4,6 +4,6 @@ locales="de es fr it ja ko zh-Hans zh-Hant"
 for i in $locales; do
     echo $i
     if [ -e $i.txt ]; then
-	iconv -f utf-8 -t utf-16 $i.txt >  ../$i.lproj/Localizable.strings
+	iconv -f UTF-8 -t UTF-16BE $i.txt >  ../$i.lproj/Localizable.strings
     fi
 done
