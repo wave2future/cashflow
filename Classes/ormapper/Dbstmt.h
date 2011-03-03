242e73108aa72857499c4c2e6ff1e7cbd11b8e54
Takuya Murakami <tmurakam@tmurakam.org>
2011-03-03 13:12:46 +0900

    split Database and Dbstmt

--- Classes/ormapper/Dbstmt.h
+++ Classes/ormapper/Dbstmt.h
@@ -37,44 +37,29 @@
 #import <UIKit/UIKit.h>
 #import <sqlite3.h>
 
-#import "DateFormatter2.h" // backward compat.
-#import "Dbstmt.h"
+@class Database;
 
 /**
-   Wrapper class of sqlite3 database
+   Wrapper class of sqlite3_stmt
 */
-@interface Database : NSObject {
-    sqlite3 *handle; ///< Database handle
-
-    NSDateFormatter *dateFormatter;
-    DateFormatter2 *dateFormatter2;
-    NSDateFormatter *dateFormatter3;
-    
-    BOOL needFixDateFormat;
+@interface dbstmt : NSObject {
+    sqlite3_stmt *mStmt;	///< sqlite3_stmt handle.
+    Database *mDb; ///< Database class handle
 }
 
-@property(nonatomic,readonly) sqlite3 *handle;
-@property(nonatomic,readonly) BOOL needFixDateFormat;
+- (id)initWithStmt:(sqlite3_stmt *)st;
+- (int)step;
+- (void)reset;
 
-+ (Database*)instance;
-+ (void)shutdown;
+- (void)bindInt:(int)idx val:(int)val;
+- (void)bindDouble:(int)idx val:(double)val;
+- (void)bindCString:(int)idx val:(const char *)val;
+- (void)bindString:(int)idx val:(NSString*)val;
+- (void)bindDate:(int)idx val:(NSDate*)date;
 
-- (id)init;
-- (void)dealloc;
-
-- (void)exec:(NSString *)sql;
-- (dbstmt*)prepare:(NSString *)sql;
-- (int)lastInsertRowId;
-
-- (void)beginTransaction;
-- (void)commitTransaction;
-- (void)rollbackTransaction;
-
-- (NSString *)dbPath:(NSString *)dbname;
-- (BOOL)open:(NSString *)dbname;
-
-// utilities
-- (NSDate*)dateFromString:(NSString *)str;
-- (NSString *)stringFromDate:(NSDate*)date;
-
+- (int)colInt:(int)idx;
+- (double)colDouble:(int)idx;
+- (const char*)colCString:(int)idx;
+- (NSString*)colString:(int)idx;
+- (NSDate*)colDate:(int)idx;
 @end
