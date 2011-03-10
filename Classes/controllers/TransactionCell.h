22967f05b3e9e613a5a4a53c34039854e0f22960
Takuya Murakami <tmurakam@tmurakam.org>
2011-03-10 14:56:44 +0900

    update LICENSE notice in header

--- Classes/controllers/TransactionCell.h
+++ Classes/controllers/TransactionCell.h
@@ -1,7 +1,10 @@
 // -*-  Mode:ObjC; c-basic-offset:4; tab-width:8; indent-tabs-mode:nil -*-
-//
+/*
+ * CashFlow for iOS
+ * Copyright (C) 2008-2011, Takuya Murakami, All rights reserved.
+ * For conditions of distribution and use, see LICENSE file.
+ */
 //  TransactionCell.h
-//
 
 #import <UIKit/UIKit.h>
 #import "AssetEntry.h"
