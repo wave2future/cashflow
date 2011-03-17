e807020bdaa02050b5e7a9c61e3018ebe57d5a82
Takuya Murakami <tmurakam@tmurakam.org>
2011-03-17 19:10:47 +0900

    add filex

---
+++ Classes/DropBoxBackup.h
@@ -0,0 +1,26 @@
+// -*-  Mode:ObjC; c-basic-offset:4; tab-width:8; indent-tabs-mode:nil -*-
+/*
+ * CashFlow for iOS
+ * Copyright (C) 2008-2011, Takuya Murakami, All rights reserved.
+ * For conditions of distribution and use, see LICENSE file.
+ */
+
+#import <UIKit/UIKit.h>
+#import "DropboxSDK.h"
+
+@interface DropBoxBackup : NSObject <DBRestClientDelegate, DBLoginControllerDelegate>
+{
+    UIViewController *mViewController;
+    DBRestClient *mRestClient;
+    int mMode;
+}
+
+@property(readonly) DBRestClient *restClient;
+
+- (void)doBackup:(UIViewController *)viewController;
+- (void)doRestore:(UIViewController *)viewController;
+
+- (void)_login;
+- (void)_exec;
+
+@end
