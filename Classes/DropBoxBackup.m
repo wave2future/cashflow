91e51e306c8895a338a86be272036b9efb007613
Takuya Murakami <tmurakam@tmurakam.org>
2011-03-17 19:06:55 +0900

    add DropBoxBackup class

--- Classes/DropBoxBackup.m
+++ Classes/DropBoxBackup.m
@@ -1,4 +1,18 @@
+// -*-  Mode:ObjC; c-basic-offset:4; tab-width:8; indent-tabs-mode:nil -*-
+/*
+ * CashFlow for iOS
+ * Copyright (C) 2008-2011, Takuya Murakami, All rights reserved.
+ * For conditions of distribution and use, see LICENSE file.
+ */
+
+#import "DropBoxBackup.h"
+#import "Database.h"
+#import "AppDelegate.h"
+
 #define	BACKUP_FILENAME	@"cashflow.db"
+
+#define MODE_BACKUP 0
+#define MODE_RESTORE 1
 
 @implementation DropBoxBackup
 
@@ -28,13 +42,13 @@
     
     // ログイン処理
     if (![session isLinked]) {
-	// 未ログイン
-	DBLoginController *controller = [[DBLoginController new] autorelease];
-	controller.delegate = self;
-	[controller presentFromController:viewController];
+        // 未ログイン
+        DBLoginController *controller = [[DBLoginController new] autorelease];
+        controller.delegate = self;
+        [controller presentFromController:mViewController];
     } else {
-	// ログイン済み
-	[self _exec];
+        // ログイン済み
+        [self _exec];
     }
 }
 
@@ -43,18 +57,16 @@
     NSString *dbPath = [[Database instance] dbPath:DBNAME];
 
     switch (mMode) {
-    case MODE_BACKUP:
-	[self.restClient
-	    uploadFile:filename
-	    toPath:@"/"
-	    fromPath:dbPath];
-	break;
+        case MODE_BACKUP:
+            [self.restClient
+             uploadFile:BACKUP_FILENAME
+             toPath:@"/"
+             fromPath:dbPath];
+            break;
 
-    case MODE_RESTORE:
-	[self.restClient
-	    loadFile:[NSString stringWithFormat:@"/%@", BACKUP_FOLDER, BACKUP_FILENAME]
-	    intoPath:dbPath];
-	break;
+        case MODE_RESTORE:
+            [self.restClient loadFile:@"/" BACKUP_FILENAME intoPath:dbPath];
+            break;
     }
 }
 
@@ -64,7 +76,7 @@
     	mRestClient = [[DBRestClient alloc] initWithSession:[DBSession sharedSession]];
     	mRestClient.delegate = self;
     }
-    return restClient;
+    return mRestClient;
 }
 
 #pragma mrk DBRestClientDelegate
@@ -92,7 +104,7 @@
 #pragma mark DBLoginControllerDelegate methods
 
 - (void)loginControllerDidLogin:(DBLoginController*)controller {
-    [self _doBackup];
+    [self _exec];
 }
 
 - (void)loginControllerDidCancel:(DBLoginController*)controller {
